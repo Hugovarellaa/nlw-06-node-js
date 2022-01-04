f22f61e59ccecf3a4d313b271303d6566d2ded24
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-01-04 17:49:13 -0300

    Delete src directory

--- src/repositories/UsersRepositories.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { User } from "../entities/entities";
-
-@EntityRepository(User)
-class UsersRepositories extends Repository<User> {}
-
-export { UsersRepositories };
