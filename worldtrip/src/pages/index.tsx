36978693a66bd42dbc8daf6e28df7b49897fea00
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-03-09 13:53:04 -0300

    slider ok

--- worldtrip/src/pages/index.tsx
+++ worldtrip/src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Banner } from "../components/Banner";
 import { Header } from "../components/Header";
 import { Heading } from "../components/Heading";
 import { Separador } from "../components/Separador";
+import { Slider } from "../components/Slider";
 import { TravelTypes } from "../components/TravelTypes";
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       <Separador />
       <Heading text="Vamos nessa?" />
       <Heading text="Então escolha seu continente" mb={["5", "14"]} />
+      <Slider/>
     </>
   );
 }
