5ed933c75e137be5d954ce89641b76525be1c1cd
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-03-09 11:15:20 -0300

    separador

---
+++ worldtrip/src/components/Separador/index.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+
+export function Separador() {
+  return (
+    <Box
+      width={["60px", "90px"]}
+      mx="auto"
+      height="2px"
+      background="gray.700"
+      my={["9", "20"]}
+    />
+  );
+}
--- worldtrip/src/pages/index.tsx
+++ worldtrip/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Banner } from "../components/Banner";
 import { Header } from "../components/Header";
+import { Separador } from "../components/Separador";
 import { TravelTypes } from "../components/TravelTypes";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Banner />
       <TravelTypes/>
+      <Separador/>
     </>
   );
 }
